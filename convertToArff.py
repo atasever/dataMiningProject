c9601ba6e7134112be3cd1e2734dc0e523166b85
M. Umut ATASEVER <umutatasever@gmail.com>
2015-04-29 21:29:05 +0300

    TID attribute is now optional

--- convertToArff.py
+++ convertToArff.py
@@ -5,18 +5,29 @@ import itertools
 def split_len(seq, length):
     return [seq[i:i+length] for i in range(0, len(seq), length)]
 
+TIDenabled = True
+
 satir = 0
 
 oku = open('converted.fasta', 'r+')
 
 helix = open('helix.arff', 'w')
-helix.write("@RELATION helix\n\n@ATTRIBUTE TID numeric\n@ATTRIBUTE aminoAcidSequence string\n\n@DATA\n")
+helix.write("@RELATION helix\n\n")
+if TIDenabled :
+    helix.write("@ATTRIBUTE TID numeric\n")
+helix.write("@ATTRIBUTE aminoAcidSequence string\n\n@DATA\n")
 
 beta = open('beta.arff', 'w')
-beta.write("@RELATION beta\n\n@ATTRIBUTE TID numeric\n@ATTRIBUTE aminoAcidSequence string\n\n@DATA\n")
+beta.write("@RELATION beta\n\n")
+if TIDenabled :
+    beta.write("@ATTRIBUTE TID numeric\n")
+beta.write("@ATTRIBUTE aminoAcidSequence string\n\n@DATA\n")
 
 loop = open('loop.arff', 'w')
-loop.write("@RELATION loop\n\n@ATTRIBUTE TID numeric\n@ATTRIBUTE aminoAcidSequence string\n\n@DATA\n")
+loop.write("@RELATION loop\n\n")
+if TIDenabled :
+    loop.write("@ATTRIBUTE TID numeric\n")
+loop.write("@ATTRIBUTE aminoAcidSequence string\n\n@DATA\n")
 
 loopTID = 0
 betaTID = 0
@@ -38,15 +49,21 @@ for line in oku:
             print segment + "-" + aminoSegment
             if segment[0]=='H' :
                 helixTID = helixTID + 1
-                helix.write(str(helixTID) + ',' + aminoSegment + '\n')
+                if TIDenabled:
+                    helix.write( str(helixTID) + ',' )
+                helix.write( aminoSegment + '\n' )
             elif segment[0]=='E' :
                 betaTID = betaTID + 1
-                beta.write(str(betaTID) + ',' + aminoSegment + '\n')
+                if TIDenabled:
+                    beta.write( str(betaTID) + ',' )
+                beta.write( aminoSegment + '\n' )
             elif segment[0]=='L' :
                 loopTID = loopTID + 1
-                loop.write(str(loopTID) + ',' + aminoSegment + '\n')
+                if TIDenabled:
+                    loop.write( str(loopTID) + ',' )
+                loop.write( aminoSegment + '\n' )
             else :
-                print "hatali ikincil yapi!!"
+                print "unexpected secondary structure : " + segment[0]
                 quit()
 
     satir = satir + 1
